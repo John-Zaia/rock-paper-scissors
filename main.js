a0af556b4428ba569c36359d1c40a3039d2a2643
John Zaia <113467748+John-Zaia@users.noreply.github.com>
2024-01-09 16:50:10 -0500

    complete first section of playRound function and create a while loop for games that end in a tie

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-console.log("Hello World!");
-
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3) + 1;
 
     if(computerChoice == 1){
-        return "rock";
+        computerChoice = "rock";
+        return computerChoice;
     }
     else if (computerChoice == 2){
-        return "paper";
+        computerChoice = "paper";
+        return computerChoice;
     }
     else{
-        return "scissors";
+        computerChoice = "scissors";
+        return computerChoice;
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    if((playerSelection == "rock") && (computerSelection == "paper")){
+        return "You Lose! Paper beats Rock";
+    }
+    else if((playerSelection == "rock") && (computerSelection == "scissors")){
+        return "You Win! Rock beats Scissors";
+    }
+    else if((playerSelection == "rock") && (computerSelection == "rock")){
+        return "It's a Tie! Play agin."
+    }
+}
+
+let playerChoice = prompt("choose either rock, paper, or scissors");
+let computerChoice = getComputerChoice();
+playerChoice = playerChoice.toLowerCase();
+
+console.log(computerChoice);
+console.log(playRound(playerChoice, computerChoice));
+
+if (playerChoice === computerChoice){
+    while(playerChoice === computerChoice){
+        playerChoice = prompt("choose either rock, paper, or scissors");
+        computerChoice = getComputerChoice();
+        console.log(playRound(playerChoice, computerChoice));
+    }
+}
